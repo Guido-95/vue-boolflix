7c87a380ce64672870e81389c383f996217745de
Guido-95 <guidocarna1995@gmail.com>
2021-06-15 18:59:09 +0200

    da sistemare grafica

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@ export default {
   }
   
   body{
-
+    padding-top: 80px;
     font-family: 'Roboto', sans-serif;
     background-color: #1b1b1b;
     color: white;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,42 @@
 <template>
     <header>
         <div class="logo">
-            <img src="../assets/logo-sito.png" alt="Logo">
+            <a href="index.html">
+                <img src="../assets/logo-sito.png" alt="Logo">
+            </a>
+            
+            <div class="Home">
+               <a href="index.html"> Home </a> 
+            </div>
+            <div>
+                Serie TV
+            </div>
+            <div>
+                Film
+            </div>
+            <div>
+                Popolari
+                </div>
+            <div>
+                La mia Lista
+            </div>
         </div>
+        
         <div class="ricerca-bottone">
             <input type="text"  @keyup.enter="cliccaBottoneRicerca" v-model="oggettoDellaRicerca" placeholder="Cerca qui..">
-            <button @click="cliccaBottoneRicerca" type="submit" value="Submit">
-                Cerca 
-            </button>
-         
+            
+            <i @click="cliccaBottoneRicerca" class="fas fa-search"></i>
            
+           <div class="caps">
+               bambini
+           </div>
+            <i class="fas fa-bell"></i>
+            <div class="avatar">
+                <img src="../assets/avatar.png" alt="">
+            </div>
            
         </div>
-
-
-        <!-- <div class="ricerca-bottone">
-            <input type="text"  @keyup.enter="cliccaBottoneRicerca" v-model="oggettoDellaRicerca" placeholder="Cerca qui..">
-            <button @click="cliccaBottoneRicerca" type="submit" value="Submit">
-                Cerca 
-            </button>
-         
-           
-           
-        </div> -->
+        
     </header>
 </template>
 
@@ -39,6 +53,7 @@ export default {
     },
     methods:{
         cliccaBottoneRicerca(){
+            
             // film
             axios
                 .get('https://api.themoviedb.org/3/search/movie',{ 
@@ -53,8 +68,8 @@ export default {
                     (risposta) => {
                     
                     this.movies = risposta.data.results;
-                   
                     this.$emit("inviaFilms", this.movies)
+                    this.oggettoDellaRicerca="";
                     }
                 )
              
@@ -69,55 +84,101 @@ export default {
             })
             .then(
                 (risposta) => {
-                  
                 this.series = risposta.data.results; 
-              
-                this.$emit("inviaSeries", this.series)
+                this.$emit("inviaSeries", this.series);
+
                 }
             )
-               
+           
         },
-  
+        
     }
 
 }
 </script>
 
 <style lang="scss" scoped>
-@import '~@fortawesome/fontawesome-free/css/all.min.css';
-
     header{
+        position: fixed;
+        top: 0;
+        z-index: 1000;
+        width: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center;
         height: 80px;
         padding: 0px 50px;
-        background-color: black;
+        
+        // background-color: transparent;
+        background-color: #1b1b1b;
     }
     .logo{
+        width: 30%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        a{
+            
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            
+        }
+        div{
+            cursor: pointer;
+            a{  
+                text-decoration: none;
+                color: white;
+            }
+        }
         img{
+            margin-right: 30px;
             width: 150px;
         }
     }
 
     .ricerca-bottone{
+        
+        width: 35%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .avatar{
+           
+        
+           
+
+            img{
+                border-radius: 5px;
+            }
+        }
         input{
-            
-            height: 40px;
-            width: spx;
-            margin: 10px;
-            background-color: #1b1b1b;
+            border-radius: 10px;
+            padding: 10px;
+            width: 400px;
+            font-size: 20px;
+            color: white;
+            background-color: #0e0e0e;
         }
 
          input::placeholder{
-            font-size: 20px;
-            color: white;
-  
+             padding: 10px;
+             font-size: 20px;
+             color: white;
+            
+            
         }
-        button{
-            height: 30px;
-            width: 50px;
+        i{
+           
+            cursor: pointer;
+            font-size: 25px;
+        }
+
+        .caps{
+            text-transform: uppercase;
         }
         
+      
     }
 </style>
